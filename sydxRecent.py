56e95b77433f26ec60bfd481d76546e12fc72cc6
FreezeRasis <79445251+FreezeRasis@users.noreply.github.com>
2022-02-20 20:16:25 +0800

    Update sydxRecent.py

--- sydxRecent.py
+++ sydxRecent.py
@@ -1,10 +1,12 @@
 import datetime
-import json
-import nonebot
+import sqlite3
 import requests
 from nonebot import on_command, CommandSession
 
+database = 'myDB.sqlite3'
 
+
+# 计算自从游玩时间到现在过了多久
 def string_toDatetime(string):
     return datetime.datetime.strptime(string, "%Y-%m-%d %H:%M:%S")
 
@@ -23,20 +25,31 @@ def timeDelta(time):
         return str(round(deltaSeconds)) + "秒前"
 
 
-@on_command('.recent', aliases='r', only_to_me=False)
+# 获取最近5个游戏记录
+@on_command('.recent', aliases='.recent', only_to_me=False)
 async def get_recent_score(session: CommandSession):
-    token = "8uwJseEBMkEQwxwlydiUuoSRmXoD1wKit8qho6rQGuEOg1xGqCz6eTvpMpmQ0pmr"
-    url = 'https://iot.universal-space.cn/api/mns/mnsGame/recordList?productId=3084&pageNo=1&pageSize=5&orderBy=gameDate'
+    # 从数据库中得到token
+    conn = sqlite3.connect(database)
+    c = conn.cursor()
+    t = str(session.ctx['user_id'])
+    c.execute('SELECT * FROM info WHERE qq = (?)', (t,))
+    token = (c.fetchone()[3])
+    conn.close()
+
+    # 获取成绩json
+    url = 'https://iot.universal-space.cn/api/mns/mnsGame/recordList?productId=3084&pageNo=1&pageSize=5&orderBy' \
+          '=gameDate '
     playdata = requests.get(url, headers={'token': token})
     json_str = playdata.json()
 
+    # json处理和发送最终生成的成绩
     i = 0
     a = "FZ.RASIS最近的游戏记录：\n"
     while i < 5:
         a += (str(json_str["data"][i]["musicName"]) + "[" +
               json_str["data"][i]["musicGradeName"].replace("NOVICE", "NOV").replace("ADVANCED", "ADV")
-              .replace("EXHAUST", "EXH").replace("MAXIMUM", "MXM").replace("INFINITE", "INF").replace("VIVID",
-                                                                                                      "VVD") + "]  " +
+              .replace("EXHAUST", "EXH").replace("MAXIMUM", "MXM")
+              .replace("INFINITE", "INF").replace("VIVID", "VVD") + "]  " +
               str(json_str["data"][i]["score"]) + "  " +
               json_str["data"][i]["clearTypeName"] + "  " +
               timeDelta(json_str["data"][i]["gameDate"]) + "\n"
@@ -45,19 +58,29 @@ async def get_recent_score(session: CommandSession):
     await session.send(a)
 
 
+# 根据所给的数字查询从现在往前推第n个成绩
 @on_command('.last', aliases='.last', only_to_me=False)
 async def get_recent_score(session: CommandSession):
-    token = "" #在这里填入token
+    # 从数据库中得到token
+    conn = sqlite3.connect(database)
+    c = conn.cursor()
+    t = str(session.ctx['user_id'])
+    c.execute('SELECT * FROM info WHERE qq = (?)', (t,))
+    token = (c.fetchone()[3])
+    conn.close()
 
+    # 获取输入的参数，没有输入或者输入错误默认为1
     num = 1
     if num:
         num = session.current_arg_text.strip()
 
+    # 获取成绩json
     url = 'https://iot.universal-space.cn/api/mns/mnsGame/recordList?productId=3084&pageNo=' + num + \
           '&pageSize=1&orderBy=gameDate'
     playdata = requests.get(url, headers={'token': token})
     json_str = playdata.json()
 
+    # json处理和发送最终生成的成绩，包含了图片
     a = "[CQ:image,file="
 
     a += (str(json_str["data"][0]["musicImage"]) + "]" +
@@ -72,5 +95,4 @@ async def get_recent_score(session: CommandSession):
           json_str["data"][0]["clearTypeName"] + "  " +
           timeDelta(json_str["data"][0]["gameDate"])
           )
-
     await session.send(a)
