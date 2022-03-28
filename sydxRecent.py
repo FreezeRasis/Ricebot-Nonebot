f815aa0286921ef10952c53e4e2df649003fa2e0
FreezeRasis <79445251+FreezeRasis@users.noreply.github.com>
2022-03-28 19:10:42 +0800

    Update sydxRecent.py
    修复了使用.rencent的时候会请求两次验证码的错误
    增加了根据难度区分封面的功能

--- sydxRecent.py
+++ sydxRecent.py
@@ -71,21 +71,20 @@ async def get_recent_score(session: CommandSession):
     playdata = requests.get(url, headers={'token': token})
     json_str = playdata.json()
     if json_str["code"] == 403:
-        get_new_token(qq)
         await session.send(get_new_token(qq))
     else:
 
         # json处理和发送最终生成的成绩
         i = 0
-        a = "[CQ:at,qq=" + qq + "]" + "最近的" + str(num) + "条游戏记录：\n"
+        a = "[CQ:at,qq=" + qq + "]" + "最近的" + str(num) + "条游戏记录："
         while i < num:
-            a += (str(json_str["data"][i]["musicName"]) + "[" +
+            a += "\n" + (str(json_str["data"][i]["musicName"]) + "[" +
                   json_str["data"][i]["musicGradeName"].replace("NOVICE", "NOV").replace("ADVANCED", "ADV")
                   .replace("EXHAUST", "EXH").replace("MAXIMUM", "MXM")
-                  .replace("INFINITE", "INF").replace("VIVID", "VVD") + "]  " +
+                  .replace("INFINITE", "INF").replace("VIVID", "VVD") + "]\n" +
                   str(json_str["data"][i]["score"]) + "  " +
                   json_str["data"][i]["clearTypeName"] + "  " +
-                  timeDelta(json_str["data"][i]["gameDate"]) + "\n"
+                  timeDelta(json_str["data"][i]["gameDate"])
                   )
             i += 1
         await session.send(a)
@@ -113,20 +112,21 @@ async def get_recent_score(session: CommandSession):
     elif json_str["pageSize"] == 0:
         await session.send("数字过大， 你可能还没有玩那么多首歌")
     else:
-
+        # json处理和发送最终生成的成绩，包含了图片
         score = json_str["data"][0]
 
         img_file = "jk_" + str(score["musicId"]).zfill(4) + "_" + str(
             score["musicGrade"] + 1) + ".png"
-        if os.path.exists(r'/usr/httpserver/img/' + img_file):
+        print(img_file)
+        if os.path.exists(r'/usr/bot/httpserver/img/' + img_file):
             img_url = "http://127.0.0.1/img/jk_" + str(score["musicId"]).zfill(4) + "_" + str(
-                score["musicGrade"] + 1) + ".png"
+            score["musicGrade"] + 1) + ".png"
         else:
             img_url = "http://127.0.0.1/img/jk_" + str(score["musicId"]).zfill(4) + "_" + str(
                 1) + ".png"
-
+        print(img_url)
         a = "[CQ:image,file=" + str(img_url
-                                    + "]\n" +
+                                    + "]" +
                                     str(score["musicName"]) + "[" +
                                     score["musicGradeName"]
                                     .replace("NOVICE", "NOV")
